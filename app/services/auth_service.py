0220ae7c56fbf6b2108c2ed330e35264210e512e
asadnavaid07 <asadnavaid34@gmail.com>
2025-04-23 22:30:41 +0500

    hash password

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,15 +1,18 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from app.schemas.auth import UserCreate
-from app.crud.user import get_user_by_email, create_user
 from fastapi import HTTPException
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User
 from app.schemas.auth import UserCreate
-from app.core.database import get_db
+import bcrypt
+from app.core.security import get_password_hash
+
+# async def get_user_by_email(db:AsyncSession,email:str):
+#     return db.query(User).filter(User.email==email).first()
 
 async def register_user(db: AsyncSession, user_data: UserCreate):
+ try:
     result = await db.execute(select(User).filter(User.email == user_data.email))
     existing_user = result.scalar()
 
@@ -17,9 +20,19 @@ async def register_user(db: AsyncSession, user_data: UserCreate):
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     
-    new_user = User(**user_data.model_dump())
+    hashed_pw = get_password_hash(user_data.password)
+
+    new_user = User(
+        email=user_data.email,
+        username=user_data.username,
+        role=user_data.role,
+        password=hashed_pw 
+    )
     db.add(new_user)
     await db.commit() 
     await db.refresh(new_user) 
     return new_user
+
+ except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
